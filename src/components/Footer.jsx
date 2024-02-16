7fba5d57983c41bea30cd77ed45b81d65a8dd03f
anastasiia-anastasieva <anastasieva.nst@gmail.com>
2024-02-16 09:52:53 +0200

    Додали Footer та MainContent

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+    return (
+        <ul className="list-group">
+            <li className="list-group-item">First item</li>
+            <li className="list-group-item">Second item</li>
+            <li className="list-group-item">Third item</li>
+        </ul>
+    );
+}
+export default Footer;
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,16 @@
+function MainContent() {
+  return (
+    <div>
+        <div className={"container"}>
+            <div className={"row"}>
+                <div className={"col"}>
+                    <h1>Main Content</h1>
+                    <p>This is the main content</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default MainContent;
