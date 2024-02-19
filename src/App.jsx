eb40f036d940938c1eccdc32171e1942fd38b65f
anastasiia-anastasieva <anastasieva.nst@gmail.com>
2024-02-19 15:04:28 +0200

    Додали Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import DanyaGeelyMK from './assets/geely.jpg';
+import MainContent from './components/MainContent.jsx';
+import Footer from './components/Footer.jsx';
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="javascript:void(0)">
+                        <img src={DanyaGeelyMK} alt="Logo" width="30" height="24"/>
+                    </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#mynavbar">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="mynavbar">
+                        <ul className="navbar-nav me-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" href="javascript:void(0)">Link</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="javascript:void(0)">Link</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="javascript:void(0)">Link</a>
+                            </li>
+                        </ul>
+                        <form className="d-flex">
+                            <input className="form-control me-2" type="text" placeholder="Search"/>
+                            <button className="btn btn-primary" type="button">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+            <MainContent/>
+            <Footer/>
+        </>
+    );
 }
 
-export default App
+export default App;
