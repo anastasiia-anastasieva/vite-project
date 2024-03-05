b86dfb507cc2bd5c716270819e0a10bac0ff335b
anastasiia-anastasieva <anastasieva.nst@gmail.com>
2024-03-05 15:06:07 +0200

    додано router

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import MainContent from './components/MainContent.jsx';
+import Home from "./components/pages/Home.jsx";
+import Contacts from "./components/pages/Contacts.jsx";
+import Gallery from "./components/pages/Gallery.jsx";
+import About from "./components/pages/About.jsx";
+import Services from "./components/pages/Services.jsx";
+import NotFound from "./components/pages/NotFound.jsx";
+
 import Footer from './components/Footer.jsx';
 import Navigation from "./components/Navigation.jsx";
-import Welcome from './components/Welcome.jsx'
-import Field from './components/Field.jsx';
-import List from './components/List.jsx';
-import TodoList from "./components/TodoList.jsx";
 import './main.css';
 
 function App() {
 
     return (
         <>
-            <Navigation/>
-
-            <Welcome name={"Anastasiia"} lastname={"Anastasieva"}/>
-            <Field label={"Введіть текст"} placeholder={"..."}/>
-            <List/>
-            <TodoList/>
-            <MainContent/>
-
-            <Footer/>
+            <BrowserRouter>
+                <Navigation />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/contacts" element={<Contacts />} />
+                    <Route path={"gallery"} element={<Gallery/>} />
+                    <Route path="/about" element={<About/>} />
+                    <Route path="/services" element={<Services />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </BrowserRouter>
+            <Footer />
         </>
     )
 }
